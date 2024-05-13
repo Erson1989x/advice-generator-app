4973d0db23bfd701848988a5141b11518cc90c73
Erhard Nagy <Erson1989x@gmail.com>
2024-05-13 11:10:00 +0300

    advice-generator-app

--- script.js
+++ script.js
@@ -7,15 +7,20 @@ const adviceId = document.querySelector(".card-container p");
 // Dice Button Function
 const diceButton = () => {
     fetch("https://api.adviceslip.com/advice")
-    .then((response) => response.json())
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error ('Network response was not ok');
+        }
+        return response.json();
+    })
     .then((data) => {
-        adviceText.textContent = data.slip.advice;
+        adviceButton.textContent = data.slip.advice;
         adviceId.textContent = `Advice #${data.slip.id}`;
-   
-    }) 
-
-}   
-
+    })
+    .catch((error) => {
+        console.log('There has been a problem with your fetch operation:', error);
+    })
+    }
 
 // Event Listener
 
